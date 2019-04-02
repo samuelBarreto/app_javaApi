95bc800e85289103f4c731c308d7670c7a40869f
Samuel Barreto de Oliveira <samukacfc1@gmail.com>
2019-04-02 19:48:39 -0300

    Update SimpleRestApisApplication.java

--- src/main/java/com/example/simplerestapis/SimpleRestApisApplication.java
+++ src/main/java/com/example/simplerestapis/SimpleRestApisApplication.java
@@ -21,7 +21,7 @@ public class SimpleRestApisApplication {
 	        return new WebMvcConfigurer() {
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
-	                registry.addMapping("/value").allowedOrigins("http://localhost:9000");
+	                registry.addMapping("/value-javaconfig").allowedOrigins("http://localhost:9000");
 	            }
 	        };
 	    }
